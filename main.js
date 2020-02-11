8458e48353cead947b03ebe30d41226b11430a71
Nguyễn Thế Sơn <55500547+Son-Nguyen48@users.noreply.github.com>
2020-02-11 20:42:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#6860d3",
+    "#68b2d3"
+  ];
+  // lets get going with the sound here
+  pads.forEach((pads, index) => {
+    pads.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubbles(index);
+    });
+  });
+
+  //Creat a function that makes bubbles
+  const createBubbles = index => {
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
